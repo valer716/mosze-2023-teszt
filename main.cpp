617d60d1b4ad10e1c3cf440146d1cd23ef218281
Valér <127779984+valer716@users.noreply.github.com>
2026-02-26 17:05:00 +0100

    hibák javítása

--- main.cpp
+++ main.cpp
@@ -4,26 +4,26 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //NELEMENTS undefined, a helyes  N_ELEMENTS lenne
-    std::cout << '1-100 ertekek duplazasa' // hibás idézőjel, '' szerepel "" helyett, std::endl; hiányzik végéről
-    for (int i = 0;) // hibás forciklus: hiányzik a ciklusfeltétel és a léptetés
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        b[i] = i * 2; //rossz számítás, (i+1)*2 kéne, ha i=0ról indulunk
+        b[i] = (i+1) * 2;
     }
-    for (int i = 0; i; i++) // ciklusfeltétel hibás
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:" // maga az érték nincs is kiírva, és std::endl; hiányzik végéről
+        std::cout << "Ertek: " << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; //nincs inicializálva
-    for (int i = 0; i < N_ELEMENTS, i++) // , helyett ; kéne
+    int atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        atlag += b[i] // ; jel hiányzik sor végéről
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
 
-    // b nincs felszabadítva
+    delete[] b;
 
     return 0;
 }
